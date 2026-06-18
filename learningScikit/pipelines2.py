1c41bf9c44116178907f9f3211bdf4b309ee7188
Armaan Khaitan <khaitanarmaan@gmail.com>
2026-06-18 19:20:43 +0100

    fds

---
+++ learningScikit/pipelines2.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.impute import SimpleImputer
+
+
+df = pd.DataFrame({
+    "Age": [21, 30, 42],
+    "Salary": [30000, 60000, 90000],
+    "Country": ["UK", "France", "Germany"],
+    "BuysProduct": [0, 1, 1]
+})
+
+
+x = df.drop(columns=["BuysProduct"])
+y = df["BuysProduct"]
+
+numeric_features = ["Age", "Salary"]
+
+categorical_features = ["Country"]
+
+
+
+numeric_pipeline = Pipeline([
+    ("imputer", SimpleImputer(strategy="mean")),
+    ("scaler", StandardScaler())
+])
+
+categorical_pipeline = Pipeline([
+    ("encoder", OneHotEncoder())
+])
+
+preprocessor = ColumnTransformer([
+    ("num", numeric_pipeline, numeric_features),
+    ("cat", categorical_pipeline, categorical_features)
+])
+
+pipe = Pipeline([
+    ("preprocessing", preprocessor),
+    ("model", LogisticRegression())
+])
+
+pipe.fit(x, y)
+
+print(pipe.predict(x))
