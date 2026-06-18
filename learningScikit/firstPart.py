45df87628b6fcd61a1265a3de0a8c74008b20d94
Armaan Khaitan <khaitanarmaan@gmail.com>
2026-06-18 16:01:28 +0100

    sfd

--- learningScikit/firstPart.py
+++ learningScikit/firstPart.py
@@ -1,14 +1,13 @@
 X_train = []
-
-for i in range(4):
+n = 10
+for i in range(n):
     X_train.append([(i+1)*100])
     
 y_train = []
 
-for i in range(4):
+for i in range(n):
     y_train.append([X_train[i][0]//2])
     
-print(y_train)
 from sklearn.linear_model import LinearRegression
 
 model = LinearRegression()
