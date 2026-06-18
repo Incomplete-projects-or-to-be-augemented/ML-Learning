4d00221df337ec058e58718801d7288a968e3b51
Armaan Khaitan <khaitanarmaan@gmail.com>
2026-06-18 18:22:19 +0100

    sfd

---
+++ learningScikit/pipelines.py
@@ -0,0 +1,28 @@
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+
+x = []
+n = 10
+for i in range(n):
+    x.append([((i+1)*10),i+1])
+    
+y = []
+
+
+for i in range(n):
+    num = random.randint(0, 1)
+    y.append(num)
+    
+X_train, X_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2
+)
+pipe = Pipeline([
+    ("scaler", StandardScaler()),
+    ("model", LogisticRegression())
+])
+
+pipe.fit(X_train, y_train)
+pred = pipe.predict(X_test)
