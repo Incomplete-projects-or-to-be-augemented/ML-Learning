31a1d544f1ebb6378c9c6c3c3b5948dcac89ad08
Armaan Khaitan <khaitanarmaan@gmail.com>
2026-06-26 17:19:58 +0100

    sdf

--- learningScikit/XGBoost.py
+++ learningScikit/XGBoost.py
@@ -3,3 +3,12 @@
 from prepData import Pipeline,XGBClassifier
 from prepData import train_test_split,accuracy_score,confusion_matrix,classification_report
 from prepData import x,y,preprocessor
+
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
