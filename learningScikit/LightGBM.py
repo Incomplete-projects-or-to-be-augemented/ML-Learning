c5bfdb8e8e9b2c03a52f378fa2f31768a7db356b
Armaan Khaitan <khaitanarmaan@gmail.com>
2026-06-26 18:54:14 +0100

    sdf

--- learningScikit/LightGBM.py
+++ learningScikit/LightGBM.py
@@ -4,7 +4,7 @@ from lightgbm import LGBMClassifier, LGBMRegressor
 
 from prepData import Pipeline
 from prepData import train_test_split, accuracy_score, confusion_matrix, classification_report
-from prepData import x, y, preprocessor
+from prepData import x, y, preprocessor,pd
 
 
 x_train, x_test, y_train, y_test = train_test_split(
@@ -19,6 +19,7 @@ model = Pipeline(
     steps=[
         ("preprocessing", preprocessor),
         ("lightgbm", LGBMClassifier(
+            boosting_type="goss",
             n_estimators=100,
             learning_rate=0.1,
             num_leaves=7,
@@ -42,3 +43,17 @@ print(confusion_matrix(y_test, y_pred))
 
 print("\nClassification Report:")
 print(classification_report(y_test, y_pred))
+
+
+lgbm = model.named_steps["lightgbm"]
+
+print(lgbm.feature_importances_)
+importance = pd.DataFrame({
+    "Feature": model.named_steps["preprocessing"].get_feature_names_out(),
+    "Importance": lgbm.feature_importances_
+}).sort_values(
+    by="Importance",
+    ascending=False
+)
+
+print(importance)
