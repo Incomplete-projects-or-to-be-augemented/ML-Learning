c32f1952cc81fe51ab25fa8adf888bfd6ed35080
Armaan Khaitan <khaitanarmaan@gmail.com>
2026-06-30 01:42:18 +0100

    sdf

---
+++ Pytorch/tensorOperations.py
@@ -0,0 +1,24 @@
+import torch
+
+a = torch.tensor([1, 2, 3])
+b = torch.tensor([10, 20, 30])
+
+print(a + b)
+print(a * b)
+
+A = torch.tensor([[1, 2],
+                  [3, 4]])
+
+B = torch.tensor([[5, 6],
+                  [7, 8]])
+print(A @ B)
+
+a = torch.tensor([1, 2, 3])
+b = 10
+
+print(a+b)
+
+x = torch.tensor([1.0, 4.0, 9.0])
+
+sqrt = torch.sqrt(x)
+mean = torch.mean(x)
