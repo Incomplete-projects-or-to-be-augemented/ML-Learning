1b98b6269f8995eb6b45e25f19f788e219682c63
Armaan Khaitan <khaitanarmaan@gmail.com>
2026-07-09 21:17:47 +0100

    sdf

--- Pytorch/reshaping Tesnors.py
+++ Pytorch/reshaping Tesnors.py
@@ -1,5 +1,5 @@
 import torch
-
+from einops import rearrange
 
 x = torch.tensor([1,2,3,4,5,6])
 
@@ -19,3 +19,12 @@ print(z)
 p = z.reshape(3,4)
 
 print(p)
+
+image = torch.rand(3, 224, 224)
+
+image_new = rearrange(
+    image,
+    'c h w -> h w c'
+)
+
+print(image_new)
